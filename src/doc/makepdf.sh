d1b2c46c323b5f2800e234608fba1fd39e6898db
minjaesong <alswo9628@gmail.com>
2021-01-04 14:52:49 +0900

    doc update to add closures

--- src/doc/makepdf.sh
+++ src/doc/makepdf.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 export LC_ALL="en_US.UTF-8"
+
+
+echo "Making pdfs from dot-graph"
+for f in *.dot; do
+  echo $f;
+  dot -Tpdf -o ${f%.*}.pdf $f;
+done
+
 lualatex tbasman.tex
 lualatex tbasman.tex
