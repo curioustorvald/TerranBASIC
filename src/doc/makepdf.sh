c49a6212ffa555f9a4ca115a3b3c450db4b280c1
minjaesong <alswo9628@gmail.com>
2021-01-06 19:20:19 +0900

    doc: index

--- src/doc/makepdf.sh
+++ src/doc/makepdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export LC_ALL="en_US.UTF-8"
 
+TEXJOBNAME=tbasman
 
 echo "Making pdfs from dot-graph"
 for f in *.dot; do
@@ -8,5 +9,6 @@ for f in *.dot; do
   dot -Tpdf -o ${f%.*}.pdf $f;
 done
 
-lualatex tbasman.tex
-lualatex tbasman.tex
+lualatex $TEXJOBNAME.tex
+makeindex $TEXJOBNAME
+lualatex $TEXJOBNAME.tex
